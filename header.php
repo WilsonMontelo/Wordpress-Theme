7a777ae10ea3e183a3b57a723311fd58b8250f0b
Wilson Montelo <contato@wilsonmontelo.com>
2017-06-17 11:59:42 -0300

    Add files via upload

--- header.php
+++ header.php
@@ -6,19 +6,23 @@
   <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
   <meta name="viewport" content="width=device-width, initial-scale=1"> 
   <title><?php bloginfo( 'name' ); ?><?php wp_title(" - ");?></title>
-
-  <!-- Bootstrap -->
-  <link href="<?php bloginfo('template_directory'); ?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Css -->
   <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <link href="<?php bloginfo('template_directory'); ?>/responsivo.css" rel="stylesheet">
+  <link href="<?php bloginfo('template_directory'); ?>/reset.css" rel="stylesheet">
+  <!-- Boostrap -->
+  <link href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css" rel="stylesheet">
   <?php wp_enqueue_script("jquery"); ?>
   <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); wp_head(); ?>
-  
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
- <header>
+<section id="menu-principal">
+  <header>
     <nav class="navbar" role="navigation">
-      <div id="centraliza" class="container-fluid">
+      <div id="centraliza" class="container">
        <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -27,6 +31,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
+        <!-- Logo -->
         <a class="logo" href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/fato-comunicacao.png" alt=""></a>
       </div>
         <?php
@@ -43,5 +48,7 @@
           );
         ?>
       </div>
-  </nav>
-</header>
+    </nav>
+  </header>
+</section>
+<!-- Fim Menu Principal -->
